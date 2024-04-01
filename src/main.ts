bb298e875497184eef67c318e5a7d9888c963708
creamlaflare <antonkazachenko2002@gmail.com>
2024-04-01 11:33:32 -0700

    Refactor Block and EventBus classes

--- src/main.ts
+++ src/main.ts
@@ -1,104 +1,208 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-
+// import Handlebars from "handlebars";
+// import * as Components from "./components";
+// import * as Pages from "./pages";
 import Block from "./tools/Block";
-import { Input } from "./components";
-const data = [
-  {
-    id: 5,
-    avatar: { size: "medium", image: "" },
-    name: "John Doe",
-    date: "Su",
-    message: "Hello, World!",
-    count: "9+",
-  },
-  {
-    id: 4,
-    avatar: { size: "medium", image: "" },
-    name: "John Doe",
-    date: "Su",
-    message: "Hello, World!",
-    count: "9+",
-  },
-  {
-    id: 3,
-    avatar: { size: "medium", image: "" },
-    name: "John Doe",
-    date: "Su",
-    message: "Hello, World!",
-    count: "9+",
-  },
-  {
-    id: 2,
-    avatar: { size: "medium", image: "" },
-    name: "John Doe",
-    date: "Su",
-    message: "Hello, World!",
-    count: "9+",
-  },
-  {
-    id: 1,
-    avatar: { size: "medium", image: "" },
-    name: "John Doe",
-    date: "Su",
-    message: "Hello, World!",
-    count: "9+",
-  },
-];
 
+// const context = {
+//   title: "Title",
+// }
+
+// const pages = {
+//   'chat': [ Pages.ChatPage ],
+//   'login': [ Pages.LoginPage, context ],
+// };
+
+// Object.entries(Components).forEach(([ name, component ]) => {
+//   Handlebars.registerPartial(name, component);
+// });
+
+// function navigate(page) {
+//   const [ source, args ] = pages[page];
+//   const handlebarsFunct = Handlebars.compile(source);
+//   const lll =  handlebarsFunct(args);
+//   console.log(lll)
+//   document.body.innerHTML = lll;
+// }
+
+// document.addEventListener('DOMContentLoaded', () => navigate('login'));
+
+// document.addEventListener('click', e => {
+//   const page = e.target.getAttribute('page');
+//   if (page) {
+//     navigate(page);
+
+//     e.preventDefault();
+//     e.stopImmediatePropagation();
+//   }
+// });
+
+/**
+ * Represents a ChatItem component, extending the generic Block component.
+ * This component is specifically designed to display a single chat message,
+ * including the sender's name and the message content.
+ */
 class ChatItem extends Block {
-  constructor(...props) {
+  /**
+   * Creates an instance of the ChatItem component with specified properties.
+   * @param {Object} props - The initial properties for the ChatItem component.
+   * @param {string} props.name - The name of the user who sent the message.
+   * @param {string} props.message - The chat message content.
+   */
+  constructor({ ...props }: { name: string; message: string }) {
     super({ ...props });
   }
 
+  /**
+   * Renders the HTML content for the ChatItem component.
+   * The method returns an HTML string representing a chat item, which includes
+   * the sender's name and the message content.
+   * @return {string} HTML string representing the chat item.
+   */
   render() {
     return `
       <div>
         <div>{{ name }}</div>
         <div>{{ message }}</div>
-      </div>
-  `;
+      </div>`;
   }
 }
 
-class Page extends Block {
-  constructor(...props) {
+/**
+ * Represents a Button component, extending the generic Block component.
+ * This component is specifically designed to handle button elements, with a click event listener.
+ */
+// class Button extends Block {
+//   /**
+//    * Creates an instance of the Button component with specific properties and event handlers.
+//    * @param {Object} props - The initial properties for the Button component, including event handlers.
+//    */
+//   constructor(props) {
+//     super({
+//       ...props,
+//       events: {
+//         click: () => console.log("event"),
+//       },
+//     });
+//   }
+//
+//   /**
+//    * Renders the HTML content for the Button component.
+//    * The method returns an HTML string representing a button element with dynamic text content.
+//    * @returns {string} HTML string representing the button element.
+//    */
+//   render() {
+//     return "<button>{{text}}</button>";
+//   }
+// }
+
+/**
+ * Represents an Input component, extending the generic Block component.
+ * This component is specifically designed to handle input elements, providing
+ * change and blur event handlers, and a validation method.
+ */
+// class Input extends Block {
+//   /**
+//    * Creates an instance of the Input component with specific properties, event handlers, and attributes.
+//    * @param {Object} props - The initial properties for the Input component.
+//    * @param {Function} props.onChange - The callback function to execute when the value of the input changes.
+//    * @param {Object} [props.attr] - Additional attributes to be added to the input element.
+//    */
+//   constructor(props) {
+//     super({
+//       ...props,
+//       events: {
+//         change: (e) => props.onChange(e.target.value),
+//         blur: (e) => this.validate(),
+//       },
+//       attr: {
+//         class: "fake",
+//       },
+//     });
+//   }
+//
+//   /**
+//    * Renders the HTML content for the Input component.
+//    * @returns {string} HTML string representing an input element.
+//    */
+//   render() {
+//     return "<input />";
+//   }
+//
+//   /**
+//    * Validates the input value. This method is called when the input loses focus (on blur event).
+//    */
+//   validate() {
+//     console.log("blur");
+//   }
+// }
+
+// class Page extends Block {
+//   constructor(props) {
+//     super({
+//       ...props, // {buttonText: 'Button'}
+//       button: new Button({ text: props.buttonText }),
+//       input: new Input({
+//         label: "input",
+//         onChange: (value) => {
+//           this.setProps({ buttonText: value });
+//         },
+//       }),
+//     });
+//   }
+//
+//   componentDidUpdate(oldProps, newProps) {
+//     if (oldProps.buttonText !== newProps.buttonText) {
+//       this.children.button.setProps({ text: newProps.buttonText });
+//     }
+//     return true;
+//   }
+//
+//   override render() {
+//     return "<div>{{{ button }}} {{{ input }}}</div>";
+//   }
+// }
+
+/**
+ * Represents a Page2 component, extending the generic Block component.
+ * This component is specifically designed to handle and display a list of ChatItem components.
+ */
+class Page2 extends Block {
+  /**
+   * Creates an instance of the Page2 component with specific properties and a list of ChatItem components.
+   * @param {Object} props - The initial properties for the Page2 component.
+   */
+  constructor(props: NonNullable<unknown>) {
     super({
-      ...props,
+      ...props, // {buttonText: 'Button'}
       lists: [
-        new ChatItem({ name: "John Doe", message: "Hello, World!" }),
-        new ChatItem({ name: "John Doe", message: "Hello, World!" }),
-        new ChatItem({ name: "John Doe", message: "Hello, World!" }),
-        new ChatItem({ name: "John Doe", message: "Hello, World!" }),
+        new ChatItem({ name: "Samanta Smith", message: "Алло, на!" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
+        new ChatItem({ name: "John Dow", message: "What?" }),
       ],
-      button: new Button({ text: props.buttonText }),
-      input: new Input({
-        label: "Input",
-        onChange: (value) => {
-          this.setProps({ buttonText: value });
-        },
-      }),
     });
   }
 
-  componentDidUpdate(oldProps, newProps): boolean {
-    if (oldProps.buttonText !== newProps.buttonText) {
-      this.children.button.setProps({ text: newProps.buttonText });
-    }
-    return true;
-  }
-
+  /**
+   * Renders the HTML content for the Page2 component.
+   * The method overrides the render method from the Block class to provide specific HTML for this component.
+   * @return {string} HTML string representing the content of the Page2 component.
+   */
   override render(): string {
-    return `
-      <div>
-        {{{ button }}}
-      </div>
-    `;
+    return "<div>{{{ lists }}}</div>";
   }
 }
 
-const block = new Page({ buttonText: "Click me!" });
-const container = document.getElementById("app");
-if (container) {
-  container.appendChild(block.getContent());
-}
+const block = new Page2();
+const container = document.getElementById("app")!;
+console.log(container);
+container.append(block.getContent()!);
--- src/tools/Block.ts
+++ src/tools/Block.ts
@@ -1,6 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-export class Block {
+import EventBus from "./EventBus";
+import Handlebars from "handlebars";
+/* eslint-disable @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any, valid-jsdoc, @typescript-eslint/no-unused-vars */
+/**
+ * Represents a generic block component in an application, managing lifecycle events, properties, and rendering.
+ */
+export default class Block {
   static EVENTS = {
     INIT: "init",
     FLOW_CDM: "flow:component-did-mount",
@@ -9,26 +15,38 @@ export class Block {
   };
 
   _element = null;
-  _meta = null;
+  _id = Math.floor(100000 + Math.random() * 900000);
 
-  /** JSDoc
-   * @param {string} tagName
-   * @param {Object} props
-   *
-   * @return {void}
+  /**
+   * Creates an instance of Block.
+   * @param {Object} propsWithChildren - The initial properties and children of the block.
    */
-  constructor(tagName = "div", props = {}) {
+  constructor(propsWithChildren: object = {}) {
     const eventBus = new EventBus();
-    this._meta = {
-      tagName,
-      props,
-    };
-    this.props = this._makePropsProxy(props);
+    const { props, children, lists } =
+      this._getChildrenPropsAndProps(propsWithChildren);
+    this.props = this._makePropsProxy({ ...props });
+    this.children = children;
+    this.lists = lists;
     this.eventBus = () => eventBus;
     this._registerEvents(eventBus);
     eventBus.emit(Block.EVENTS.INIT);
   }
 
+  /**
+   * Registers the DOM events listed in the block's properties.
+   */
+  _addEvents() {
+    const { events = {} } = this.props;
+    Object.keys(events).forEach((eventName) => {
+      this._element.addEventListener(eventName, events[eventName]);
+    });
+  }
+
+  /**
+   * Registers block events with the event bus.
+   * @param {EventBus} eventBus - The event bus to register the block's events with.
+   */
   _registerEvents(eventBus) {
     eventBus.on(Block.EVENTS.INIT, this.init.bind(this));
     eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
@@ -36,88 +54,88 @@ export class Block {
     eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
   }
 
-  _createResources() {
-    const { tagName } = this._meta;
-    this._element = this._createDocumentElement(tagName);
-  }
-
+  /**
+   * Initializes the block by emitting the FLOW_RENDER event.
+   */
   init() {
-    this._createResources();
-
     this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
   }
 
+  /**
+   * Handles the component did mount lifecycle event.
+   */
   _componentDidMount() {
     this.componentDidMount();
+    Object.values(this.children).forEach((child) => {
+      child.dispatchComponentDidMount();
+    });
   }
 
-  componentDidMount(oldProps) {}
+  /**
+   * Placeholder for the componentDidMount lifecycle method which can be overridden.
+   * @param {Object} oldProps - The previous properties of the block.
+   */
+  componentDidMount(oldProps: object) {}
 
+  /**
+   * Dispatches the component did mount event for this block.
+   */
   dispatchComponentDidMount() {
     this.eventBus().emit(Block.EVENTS.FLOW_CDM);
   }
 
-  _componentDidUpdate(oldProps, newProps) {
+  /**
+   * Handles the component did update lifecycle event.
+   * @param {Object} oldProps - The old properties of the block.
+   * @param {Object} newProps - The new properties of the block.
+   * @return {boolean} Whether the component should re-render.
+   */
+  _componentDidUpdate(oldProps: object, newProps: object): boolean {
+    console.log("_componentDidUpdate");
     const response = this.componentDidUpdate(oldProps, newProps);
     if (!response) {
       return;
     }
     this._render();
+    return true;
   }
 
+  /**
+   * Placeholder for the componentDidUpdate lifecycle method which can be overridden.
+   * @param {Object} oldProps - The old properties of the block.
+   * @param {Object} newProps - The new properties of the block.
+   * @return {boolean} Whether the component should re-render.
+   */
   componentDidUpdate(oldProps, newProps) {
     return true;
   }
 
-  setProps = (nextProps) => {
-    if (!nextProps) {
-      return;
-    }
+  /**
+   * Separates children blocks, lists, and props from a combined object.
+   * @param {Object} propsAndChildren - The combined properties and children.
+   * @return {Object} The separated properties, children, and lists.
+   */
+  _getChildrenPropsAndProps(propsAndChildren) {
+    const children = {};
+    const props = {};
+    const lists = {};
 
-    Object.assign(this.props, nextProps);
-  };
-
-  get element() {
-    return this._element;
-  }
-
-  _render() {
-    const propsAndStubs = { ...this.props };
-    Object.entries(this.children).forEach(([key, child]) => {
-      propsAndStubs[key] = `<div data-id="${child._id}"></div>`;
+    Object.entries(propsAndChildren).forEach(([key, value]) => {
+      if (value instanceof Block) {
+        children[key] = value;
+      } else if (Array.isArray(value)) {
+        lists[key] = value;
+      } else {
+        props[key] = value;
+      }
     });
 
-    if (this.lists) {
-      Object.entries(this.lists).forEach(([key, child]) => {
-        propsAndStubs[key] = `<div data-id="__l_${child._id}"></div>`;
-      });
-    }
-
-    const fragment = this._createDocumentElement("template");
-    fragment.innerHTML = Handlebars.compile(this.render())(propsAndStubs);
-    const fragmentChildren = fragment.content.firstElementChild;
-
-    Object.entries(this.children).forEach(([key, child]) => {
-      const child = fragmentChildren.querySelector(`[data-id="${child._id}"]`);
-      child.replaceWith(value.getContent());
-    });
-
-    if (this.lists) {
-      Object.entries(this.lists).forEach(([key, child]) => {
-        const listCont = this._createDocumentElement("template");
-
-        child.forEach((item) => {});
-      });
-    }
-
-    if (this._element) {
-      this._element.replaceWith(fragmentChildren);
-    }
-    this._element = newElement;
-    this._addEvents();
-    this.addAttributes();
+    return { children, props, lists };
   }
 
+  /**
+   * Adds attributes to the block's root element based on the `attr` property.
+   */
   addAttributes() {
     const { attr = {} } = this.props;
 
@@ -126,48 +144,126 @@ export class Block {
     });
   }
 
+  /**
+   * Updates the block's properties with new values.
+   * @param {Object} nextProps - The new properties to be set.
+   */
+  setProps(nextProps) {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  }
+
+  /**
+   * Gets the root element of the block.
+   * @return {HTMLElement} The root HTML element of the block.
+   */
+  get element() {
+    return this._element;
+  }
+
+  /**
+   * Renders the block's content, updates the DOM, and binds events.
+   */
+  _render() {
+    const propsAndStubs = { ...this.props };
+    const _tmpId = Math.floor(100000 + Math.random() * 900000);
+    Object.entries(this.children).forEach(([key, child]) => {
+      propsAndStubs[key] = `<div data-id="${child._id}"></div>`;
+    });
+
+    Object.entries(this.lists).forEach(([key, _]) => {
+      propsAndStubs[key] = `<div data-id="__l_${_tmpId}"></div>`;
+    });
+
+    const fragment = this._createDocumentElement("template");
+    fragment.innerHTML = Handlebars.compile(this.render())(propsAndStubs);
+
+    Object.values(this.children).forEach((child) => {
+      const stub = fragment.content.querySelector(`[data-id="${child._id}"]`);
+      stub.replaceWith(child.getContent());
+    });
+
+    Object.entries(this.lists).forEach(([, list]) => {
+      const listContainer = this._createDocumentElement("template");
+      list.forEach((item) => {
+        if (item instanceof Block) {
+          listContainer.content.append(item.getContent());
+        } else {
+          listContainer.content.append(`${item}`);
+        }
+      });
+      const stub = fragment.content.querySelector(`[data-id="__l_${_tmpId}"]`);
+      stub.replaceWith(listContainer.content);
+    });
+
+    const newElement = fragment.content.firstElementChild;
+    if (this._element) {
+      this._element.replaceWith(newElement);
+    }
+    this._element = newElement;
+    this._addEvents();
+  }
+
+  /**
+   * Placeholder for the render method, which should be overridden to return HTML content as a string.
+   * @return {string} The HTML content to render.
+   */
   render() {}
 
+  /**
+   * Retrieves the content of the block, which is its root element.
+   * @return {HTMLElement} The root element of the block.
+   */
   getContent() {
     return this.element;
   }
 
-  _makePropsProxy(props) {
-    // Можно и так передать this
-    // Такой способ больше не применяется с приходом ES6+
-    const self = this;
-
+  /**
+   * Creates a proxy around the block's props to enable reactivity.
+   * @param {Object} props - The initial props object.
+   * @return {Proxy} A proxy that surrounds the props.
+   */
+  __makePropsProxy(props) {
     return new Proxy(props, {
-      get(target, prop) {
+      get: (target, prop) => {
         const value = target[prop];
         return typeof value === "function" ? value.bind(target) : value;
       },
-      set(target, prop, value) {
+      set: (target, prop, value) => {
+        const oldTarget = { ...target };
         target[prop] = value;
-
-        // Запускаем обновление компоненты
-        // Плохой cloneDeep, в следующей итерации нужно заставлять добавлять cloneDeep им самим
-        self.eventBus().emit(Block.EVENTS.FLOW_CDU, { ...target }, target);
+        this.eventBus().emit(Block.EVENTS.FLOW_CDU, oldTarget, target);
         return true;
       },
-      deleteProperty() {
-        throw new Error("Нет доступа");
+      deleteProperty: () => {
+        throw new Error("No access");
       },
     });
   }
 
-  _createDocumentElement(tagName) {
-    // Можно сделать метод, который через фрагменты в цикле создаёт сразу несколько блоков
+  /**
+   * Creates a new HTML element with the specified tag name.
+   * @param {string} tagName - The tag name for the new element.
+   * @return {Element} The new HTML element.
+   */
+  _createDocumentElement(tagName: string) {
     return document.createElement(tagName);
   }
 
+  /**
+   * Shows the block by setting its display style to 'block'.
+   */
   show() {
     this.getContent().style.display = "block";
   }
 
+  /**
+   * Hides the block by setting its display style to 'none'.
+   */
   hide() {
     this.getContent().style.display = "none";
   }
 }
-
-export default Block;
