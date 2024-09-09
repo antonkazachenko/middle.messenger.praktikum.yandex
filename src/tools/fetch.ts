4dbbc6da7e89a6e55c662c653d9120625e28729e
creamlaflare <antonkazachenko2002@gmail.com>
2024-09-08 18:53:23 -0700

    add HTTPTransport class

--- src/tools/fetch.ts
+++ src/tools/fetch.ts
@@ -1,22 +1,66 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// function fetch(url: string, options) {
-//   return new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open(options.method, url);
-//     xhr.withCredentials = true;
-//
-//     xhr.onload = function() {
-//       if (xhr.status >= 200 && xhr.status < 300) {
-//         resolve(xhr.response);
-//       } else {
-//         reject(xhr.statusText);
-//       }
-//     };
-//
-//     xhr.onerror = function() {
-//       reject(xhr.statusText);
-//     };
-//
-//     xhr.send(options.body);
-//   });
-// }
+enum METHOD {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  PATCH = "PATCH",
+  DELETE = "DELETE"
+}
+
+type Options = {
+  method: METHOD;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data?: any;
+};
+
+type OptionsWithoutMethod = Omit<Options, "method">;
+
+class HTTPTransport {
+  get(url: string, options: OptionsWithoutMethod = {}):
+    Promise<XMLHttpRequest> {
+    return this.request(url, {...options, method: METHOD.GET});
+  }
+
+  post(url: string, options: OptionsWithoutMethod = {}):
+    Promise<XMLHttpRequest> {
+    return this.request(url, {...options, method: METHOD.POST});
+  }
+
+  put(url: string, options: OptionsWithoutMethod = {}):
+    Promise<XMLHttpRequest> {
+    return this.request(url, {...options, method: METHOD.PUT});
+  }
+
+  patch(url: string, options: OptionsWithoutMethod = {}):
+    Promise<XMLHttpRequest> {
+    return this.request(url, {...options, method: METHOD.PATCH});
+  }
+
+  delete(url: string, options: OptionsWithoutMethod = {}):
+    Promise<XMLHttpRequest> {
+    return this.request(url, {...options, method: METHOD.DELETE});
+  }
+
+  request(url: string, options: Options = {method: METHOD.GET}):
+    Promise<XMLHttpRequest> {
+    const {method, data} = options;
+
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+
+      xhr.onload = function() {
+        resolve(xhr);
+      };
+
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      if (method === METHOD.GET || !data) {
+        xhr.send();
+      } else {
+        xhr.send(data);
+      }
+    });
+  }
+}
